221dc49964b97b75c6b21bdcd806c62584a583bb
lukaszkapciak <lukaszkapciakup@gmail.com>
2018-03-19 00:50:43 +0100

    Commit {T7} - Add recalculate Ct function. Add recalculate ΔCts MeanCts function. Add "step-back" function for recalculation. Visual corrections, added elements in view. Oganize the code. Install AJAX for a project.

--- BioSite/Controllers/CalculateController.cs
+++ BioSite/Controllers/CalculateController.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
-using WebGrease.Css.Extensions;
+using LogicBioSite.Models.CalculateCts;
 
 namespace BioSite.Controllers
 {
@@ -20,11 +20,6 @@ namespace BioSite.Controllers
             _ICalculateCtLogic = calculateCtLogic;
         }
         #endregion
-
-        public ActionResult Index()
-        {
-            return View();
-        }
 
         public ActionResult CalculateCt()
         {
@@ -46,6 +41,7 @@ namespace BioSite.Controllers
             try
             {
                 var data = _ICalculateCtLogic.CalculateCt(rMax, digits, predicted, Session["userCurrentData"] as IEnumerable<AmplificationData>);
+                Session["CalculatedCtsΔCtsmeanCts"] = data;
                 return View("CalculateCt", data);
             }
             catch (Exception e)
@@ -53,6 +49,58 @@ namespace BioSite.Controllers
 
                 throw;
             }
+        }
+
+        [HttpPost]
+        public JsonResult RecalculateCt(string newCtWell, string newCtMiRname, string newCtInputId, double lessX, double greaterX, double lessY, double greaterY, double treshold, ulong predicted)
+        {
+            try
+            {
+                var oldData = Session["CalculatedCtsΔCtsmeanCts"] as CtViewModel;
+                var oldCts = oldData?.Cts.Select(item => new CalculateCt() {Ct = item.Ct, readValues = item.readValues, ThresholdValue = item.ThresholdValue, Well = item.Well, meanCt = item.meanCt, ΔCt = item.ΔCt, id = item.id, miRname = item.miRname}).ToList();
+                var previousState = new CtViewModel { Cts = oldCts, Mean = oldData.Mean, StandardDeviation = oldData.StandardDeviation };
+                Session["CalculatedCtsΔCtsmeanCtsPreviousState"] = previousState;
+                var ctValue = _ICalculateCtLogic.ReCalculateCt(lessX, greaterX, lessY, greaterY, treshold, predicted);
+                
+                oldData.Cts.Single(p => p.Well.Equals(newCtWell)).Ct = ctValue;
+                Session["CalculatedCtsΔCtsmeanCts"] = oldData;
+
+                return Json(new { newCtInputId, newCtWell, newCtMiRname, ctValue = ctValue.ToString() });
+            }
+            catch (Exception e)
+            {
+                return Json(new { message = e });
+            }
+        }
+
+        public ActionResult ReCalculateCtsDeltaCtsmeanCts()
+        {
+            var data = Session["CalculatedCtsΔCtsmeanCts"] as CtViewModel;
+
+            if (data != null)
+            {
+                var meanCts = data.Cts.Where(p => p.Ct > 0).Select(p => p.Ct).Average();
+                var stnDev = Math.Sqrt(data.Cts.Where(p => p.Ct > 0).Select(p => p.Ct).Average(v => Math.Pow(v - data.Cts.Where(p => p.Ct > 0).Select(p => p.Ct).Average(), 2)));
+                foreach (var item in data.Cts.Where(p => p.Ct > 0))
+                {
+                    item.ΔCt = item.Ct - meanCts;
+                    item.meanCt = Math.Pow(2, -item.ΔCt);
+                }
+
+                data.Mean = meanCts;
+                data.StandardDeviation = stnDev;
+            }
+
+            Session["CalculatedCtsΔCtsmeanCts"] = data;
+
+            return View("CalculateCt", data);
+        }
+
+        public ActionResult ReadPreviousState()
+        {
+            var data = Session["CalculatedCtsΔCtsmeanCtsPreviousState"] as CtViewModel;
+            Session["CalculatedCtsΔCtsmeanCts"] = data;
+            return View("CalculateCt", data);
         }
     }
 }
